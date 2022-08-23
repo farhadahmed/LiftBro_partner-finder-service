7a566acb2ea7dfe3e99cd2ab46203712a33148b0
Farhad Ahmed <farhadahmed2007@gmail.com>
2022-08-23 00:59:01 -0700

    first commit

---
+++ src/main/java/com/liftbro/partnerfinderservice/PartnerFinderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.liftbro.partnerfinderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartnerFinderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartnerFinderServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/liftbro/partnerfinderservice/PartnerFinderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liftbro.partnerfinderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartnerFinderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
